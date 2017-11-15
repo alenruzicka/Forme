461b0f0a0919ebfa2540a16e51112fae0276efce
Alen Ruzicka <alenruzicka111@gmail.com>
2017-11-15 12:55:32 +0100

    Dodali smo nove datoteke

---
+++ rezultat.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<title></title>
+<link rel="stylesheet" type ="text.css" href="style.css">
+</head>
+
+<body>
+<?php
+//print_r($_POST);
+echo('Pozdrav, ' . $_POST['prezime'] . $_POST['ime']);
+?>
+</body>
+
+</html>
